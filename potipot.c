27f13ed32ddd4e1463f8277df7c593ba83a1b5ab
Malaury <malaumalau60@gmail.com>
2019-06-12 09:34:32 +0200

    change main

--- potipot.c
+++ potipot.c
@@ -21,87 +21,7 @@ typedef struct sockaddr SOCKADDR;
 char * IP = ("127.0.0.1");
 #define PORT 5000
 int erreur = 0;
-
-void sendNewState(SOCKET csock){
-	int code = analyzeWatering();
-	char* buffer = code;
-	
-	int sock_err = send(csock,buffer, 300, 0);
-	if(sock_err != SOCKET_ERROR)
-		printf("State envoyé : %s\n", buffer);
-	else
-		printf("ERROR - Erreur de transmission \n");
-	return;
-}
-
-int main(int argc, char* argv[]){
-	/* Socket et contexte d'adressage du serveur */
-	SOCKADDR_IN sin;
-	SOCKET sock;
-	socklen_t recsize = sizeof(sin);
-	    
-	/* Socket et contexte d'adressage du client */
-	SOCKADDR_IN csin;
-	SOCKET csock;
-	socklen_t crecsize = sizeof(csin);
-	    
-	int sock_err;
-	char buffer[300];
-	    
-	    
-	if(!erreur)
-	{
-		/* Création d'une socket */
-		sock = socket(AF_INET, SOCK_STREAM, 0);
-		
-		/* Si la socket est valide */
-		if(sock != INVALID_SOCKET)
-		{
-		    printf("La socket %d est maintenant ouverte en mode TCP/IP\n", sock);
-		    
-		    /* Configuration */
-		    sin.sin_addr.s_addr = inet_addr(IP);  /* Adresse IP automatique */
-		    sin.sin_family = AF_INET;                 /* Protocole familial (IP) */
-		    sin.sin_port = htons(PORT);               /* Listage du port */
-		    sock_err = bind(sock, (SOCKADDR*)&sin, recsize);
-		    
-		    /* Si la socket fonctionne */
-		    if(sock_err != SOCKET_ERROR)
-		    {
-		        /* Démarrage du listage (mode server) */
-		        sock_err = listen(sock, 5);
-		        printf("Listage du port %d...\n", PORT);
-		        
-		        /* Si la socket fonctionne */
-		        if(sock_err != SOCKET_ERROR)
-		        {
-		            /* Attente pendant laquelle le client se connecte */
-		            printf("Patientez pendant que le client se connecte sur le port %d...\n", PORT);
-		            csock = accept(sock, (SOCKADDR*)&csin, &crecsize);
-		            printf("Un client se connecte avec la socket %d de %s:%d\n", csock, inet_ntoa(csin.sin_addr), htons(csin.sin_port));
-			    while(recv(sock, buffer, 300, 0) != SOCKET_ERROR)
-				printf("Reçu : %s\n", buffer);
-			    sendNewState(csock);
-		        }
-		        else
-		            perror("listen");
-		    }
-		    else
-		        perror("bind");
-		    
-		    /* Fermeture de la socket client et de la socket serveur */
-		    printf("Fermeture de la socket client\n");
-		    closesocket(csock);
-		    printf("Fermeture de la socket serveur\n");
-		    closesocket(sock);
-		    printf("Fermeture du serveur terminée\n");
-		}
-	else
-		perror("socket");
-	}
-	    
-	    return EXIT_SUCCESS;
-}
+MYSQL mysql;
 
 void saveWatering(MYSQL * mysql, int humidity){
 	mysql_query(mysql,"INSERT INTO spray (degree, humidity) values (20, 500)");
@@ -166,10 +86,6 @@ int getDayBetweenWatering(MYSQL * mysql)
 
 int analyzeWatering(){
 	
-	/* Variable BDD et option */
-	MYSQL mysql;
-	bddConnection(&mysql);
-	
 	/* Récupération du dernier arrosage */
 	char* lastDateSpray= getDateLastSpray(&mysql);
 	
@@ -198,5 +114,96 @@ int analyzeWatering(){
 	}
 }
 
+void sendNewState(SOCKET csock){
+	int code = analyzeWatering();
+	char buffer[4];
+	sprintf(buffer,"%d",code);
+	
+	int sock_err = send(csock,buffer, 300, 0);
+	if(sock_err != SOCKET_ERROR)
+		printf("State envoyé : %d\n", buffer[0]);
+	else
+		printf("ERROR - Erreur de transmission \n");
+	return;
+}
+
+int main(int argc, char* argv[]){
+	/* Socket et contexte d'adressage du serveur */
+	SOCKADDR_IN sin;
+	SOCKET sock;
+	socklen_t recsize = sizeof(sin);
+	    
+	/* Socket et contexte d'adressage du client */
+	SOCKADDR_IN csin;
+	SOCKET csock;
+	socklen_t crecsize = sizeof(csin);
+
+	/* Variable BDD et option */
+	bddConnection(&mysql);
+	    
+	int sock_err;
+	char buffer[300];
+	    
+	    
+	if(!erreur)
+	{
+		/* Création d'une socket */
+		sock = socket(AF_INET, SOCK_STREAM, 0);
+		
+		/* Si la socket est valide */
+		if(sock != INVALID_SOCKET)
+		{
+		    printf("La socket %d est maintenant ouverte en mode TCP/IP\n", sock);
+		    
+		    /* Configuration */
+		    sin.sin_addr.s_addr = inet_addr(IP);  /* Adresse IP automatique */
+		    sin.sin_family = AF_INET;                 /* Protocole familial (IP) */
+		    sin.sin_port = htons(PORT);               /* Listage du port */
+		    sock_err = bind(sock, (SOCKADDR*)&sin, recsize);
+		    
+		    /* Si la socket fonctionne */
+		    if(sock_err != SOCKET_ERROR)
+		    {
+		        /* Démarrage du listage (mode server) */
+		        sock_err = listen(sock, 5);
+		        printf("Listage du port %d...\n", PORT);
+		        
+		        /* Si la socket fonctionne */
+		        if(sock_err != SOCKET_ERROR)
+		        {
+		            /* Attente pendant laquelle le client se connecte */
+		            printf("Patientez pendant que le client se connecte sur le port %d...\n", PORT);
+		            csock = accept(sock, (SOCKADDR*)&csin, &crecsize);
+		            printf("Un client se connecte avec la socket %d de %s:%d\n", csock, inet_ntoa(csin.sin_addr), htons(csin.sin_port));
+			    while(recv(sock, buffer, 300, 0) != SOCKET_ERROR){
+				printf("Reçu : %s\n", buffer);
+				int humidity;
+				int code;
+				sscanf(buffer, "%d:%d",code, humidity);
+				if(code == 301){
+					saveWatering(&mysql, humidity);
+				}
+			    	sendNewState(csock);
+				}
+		        }
+		        else
+		            perror("listen");
+		    }
+		    else
+		        perror("bind");
+		    
+		    /* Fermeture de la socket client et de la socket serveur */
+		    printf("Fermeture de la socket client\n");
+		    closesocket(csock);
+		    printf("Fermeture de la socket serveur\n");
+		    closesocket(sock);
+		    printf("Fermeture du serveur terminée\n");
+		}
+	else
+		perror("socket");
+	}
+	    
+	    return EXIT_SUCCESS;
+}
 
 
